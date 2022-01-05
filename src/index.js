9ea0d0a569b13008e2221d61397121383d77f65f
leshiy77 <rifbenya@gmail.com>
2022-01-05 20:33:39 +0200

    feat: add code task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+  reverseNum = [];
+  num = n;
+  if (n > 0) {
+    for (let i = 0; i < String(n).length; i++) {
+        reverseNum.push(num % 10);
+        num = (num - num % 10) / 10;
+    }
+  } else {
+      num = Math.abs(n)
+      for (let i = 0; i < String(n).length -1; i++) {
+        reverseNum.push(num % 10);
+        num = (num - num % 10) / 10;
+      }
+      return `+${Number(reverseNum.join(''))}`;
+  }
+  return Number(reverseNum.join(''));
 }
